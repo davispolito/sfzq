13436392d657e29556ddca44037e72415dfa3429
Davis Polito <davispolito1@gmail.com>
2024-08-02 16:00:27 -0400

    using chowdsp cmake setup to create test environment for sfzero module

---
+++ SFZPlayer/SFZSamplerSound.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Davis Polito on 8/2/24.
+//
+
+#include "SFZSamplerSound.h"
---
+++ SFZPlayer/tests/sfzero_tests/sf_tests/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Davis Polito on 8/2/24.
+//
+
+#include <CatchUtils.h>
+#include <sfzero_juce/sfzero_juce.h>
+
+
+TEST_CASE ("Version Test", "[plugin][version]")
+{
+
+
+}
